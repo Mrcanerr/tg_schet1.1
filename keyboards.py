2eee998cd2c919032e95d9e7f55fb19a6429e6c1
Mrcanerr <146875965+Mrcanerr@users.noreply.github.com>
2026-02-11 20:02:58 +0300

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -5,5 +5,6 @@ main_kb = ReplyKeyboardMarkup(resize_keyboard=True)
 btn1 = KeyboardButton("Загрузить файл")
 btn2 = KeyboardButton("Получить отчет")
 btn3 = KeyboardButton("Как должен выглядеть файл")
+btn_miss = KeyboardButton("Показать несовпадения")
 
 main_kb.add(btn1, btn2, btn3)
