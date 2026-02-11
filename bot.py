581e17d89b925e4eba1698315ebc7ccea5c473d7
Mrcanerr <146875965+Mrcanerr@users.noreply.github.com>
2026-02-11 19:20:47 +0300

    bot.py

--- bot.py
+++ bot.py
@@ -1,13 +1,10 @@
-import os
 from aiogram import Bot, Dispatcher, executor, types
 from handlers.start import cmd_start, file_help
 from handlers.file_handler import handle_file
 
-TOKEN = os.getenv("8464230833:AAHuVdH301Oh2vNEplUpYPHlWLYtlQEBZzk")
+TOKEN = "8464230833:AAHuVdH301Oh2vNEplUpYPHlWLYtlQEBZzk"
 
 bot = Bot(token=TOKEN)
-dp = Dispatcher(bot)
-
 dp = Dispatcher(bot)
 
 dp.register_message_handler(cmd_start, commands=["start"])
@@ -16,5 +13,3 @@ dp.register_message_handler(handle_file, content_types=types.ContentType.DOCUMEN
 
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
-
-
